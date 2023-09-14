f865c0cac122de9d711dc9978a4ec4cd62e9354f
afiqidham <afiq.zaki25@gmail.com>
2023-09-14 19:10:28 +0800

    assignment 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
-
-  function formatUser(user) {
-    return user.firstname + " " + user.lastname;
-  }
-
-  function getGreeting(user) {
-    if(user) {
-      return <h1>Hello, {formatUser(user)}</h1>
-    }
-    return <h1>Hello, Stranger</h1>
-  }
   const user = {
     firstname: "Afiq", 
     lastname: "Idham",
   };
-  const element = <h1>Hello World</h1>
+
   return <div>
-    {getGreeting(user)}
-  </div>;
+    <h1>Time Now:</h1>
+    <h2>{Clock()}</h2>
+    </div>;
 }
 
 export default App;
